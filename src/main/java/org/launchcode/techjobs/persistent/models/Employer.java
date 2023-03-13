68ac79de492516dfe72ba9c4094dfde5095fca68
Matthew Creek <matthew.creek27@gmail.com>
2023-03-13 16:12:58 -0500

    part 3 number 4 in process

--- src/main/java/org/launchcode/techjobs/persistent/models/Employer.java
+++ src/main/java/org/launchcode/techjobs/persistent/models/Employer.java
@@ -1,8 +1,12 @@
 package org.launchcode.techjobs.persistent.models;
 
 import javax.persistence.Entity;
+import javax.persistence.JoinColumn;
+import javax.persistence.OneToMany;
 import javax.validation.constraints.NotBlank;
 import javax.validation.constraints.Size;
+import java.util.ArrayList;
+import java.util.List;
 
 @Entity
 public class Employer extends AbstractEntity {
@@ -10,6 +14,10 @@ public class Employer extends AbstractEntity {
     @NotBlank(message = "Field cannot be blank.")
     @Size(min = 3, max = 100, message = "Location must be at least 3 characters.")
     private String location;
+
+    @OneToMany
+    @JoinColumn(name = "employer_id")
+    private List<Job> jobs = new ArrayList<>();
 
     //Unsure whether this constructor is needed or is correct
     public Employer(String location){
