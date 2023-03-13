88b20cb8e881eb76f17b3c1553e784b5a138d4a2
Matthew Creek <matthew.creek27@gmail.com>
2023-03-13 02:03:36 -0500

    added repositories for skills and employers, working on getting validation working in view

--- queries.sql
+++ queries.sql
@@ -1,5 +1,9 @@
 -- Part 1: Test it with SQL
-
+-- techjobs colums:
+-- int id
+-- string employer
+-- string name
+-- string skills
 -- Part 2: Test it with SQL
 
 -- Part 3: Test it with SQL
--- src/main/java/org/launchcode/techjobs/persistent/models/Employer.java
+++ src/main/java/org/launchcode/techjobs/persistent/models/Employer.java
@@ -1,6 +1,28 @@
 package org.launchcode.techjobs.persistent.models;
 
+import javax.persistence.Entity;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.Size;
+
+@Entity
 public class Employer extends AbstractEntity {
 
+    @NotBlank(message = "Field cannot be blank.")
+    @Size(min = 3, max = 100, message = "Location must be at least 3 characters.")
+    private String location;
 
+    //Unsure whether this constructor is needed or is correct
+    public Employer(String location){
+        this.location = location;
+    }
+
+    public Employer() {};
+
+    public String getLocation() {
+        return location;
+    }
+
+    public void setLocation(String location) {
+        this.location = location;
+    }
 }
